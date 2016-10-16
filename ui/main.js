8e314b69742f0bbb6028b88b56467ae8c7f9b3dd
AKHILAPARAMESWARAN <akhilaparameswaran1996@gmail.com>
2016-10-16 15:59:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,13 +9,13 @@ button.onclick=function(){
     request.onreadystatechange=function(){
         if(request.readyState===XMLHttpRequest.DONE){
         //Take some action
-        if(request.status===200){
-           var counter= request.responseText;
-           var span=document.getElementById('count');
-           span.innerHTML=counter.toString();
-        }
+            if(request.status===200){
+               var counter= request.responseText;
+               var span=document.getElementById('count');
+               span.innerHTML=counter.toString();
+            }
         
-      }
+        }
       //Not done yet
     };
    //Make the request 
